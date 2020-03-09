426f02da52d5b28e8c912f7e6f5c5069eb5b1b8b
Wayne Hofstra <wayne.hofstra@hva.nl>
2020-03-09 22:49:02 +0100

    Added Dialogue Stack Class

---
+++ Assets/Scripts/DialogStack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogStack : MonoBehaviour
+{
+    [SerializeField]
+    private Text _field;
+
+    private string[] names = new string[1];
+
+    void Awake()
+    {
+        var dialogSystem = GetComponent<DialogSystem>();
+        dialogSystem.DialogChanged += OnDialogStack;
+
+        names[0] = "B. Weij";
+    }
+
+    private void OnDialogStack(string newDialog)
+    {
+        _field.text += newDialog + " [" + names[0] + " at: " + System.DateTime.Now + "]\n";
+    }
+}
